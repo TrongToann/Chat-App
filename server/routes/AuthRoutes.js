2304ce382718b15bf4a0d1e561d216f9a0d76275
Nguyễn Trọng Toàn <93824845+TrongToann@users.noreply.github.com>
2023-09-07 10:15:35 +0700

    Delete server directory

--- server/routes/AuthRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import authRouter from "../controllers/AuthController";
-const router = express.Router();
-
-router.post("/", authRouter);
-
-module.exports = router;
--- server/routes/main.router.js
+++
@@ -1,5 +0,0 @@
-import AuthRouter from "../routes/AuthRoutes";
-function router(app) {
-  app.use("/", AuthRouter);
-}
-module.exports = router;
